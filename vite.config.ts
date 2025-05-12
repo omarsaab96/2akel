923ca069d36818155a04aec215d5340930e633c4
omarsaab96 <omar.saab.gwy.iq>
2025-05-12 13:35:32 +0300

    .

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/2akel/',
+  base: '/',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
